64f3648c41df2564d2e6aaea6315ea3a6b350bcf
daincoding <official.toyou@gmail.com>
2025-08-25 10:52:11 +0200

    Second Structure Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+
+
+function App() {
+
+
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    )
+}
+
+export default App
